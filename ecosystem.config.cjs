e92fe0ef2b3dcdfa567a458b438c87b521a745ed
work <nikita@MacBookPro.domrurouter.net>
2025-08-13 17:19:57 +0500

    last

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: 'laravel-queue',
-      script: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd/queue-worker.sh',
+      name: 'laravel-queue-wallet',
+      script: 'php',
+      args: ['artisan', 'queue:work', '--queue=wallet-generation', '--verbose', '--tries=3', '--max-time=3600'],
+      cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+      instances: 1,
+      exec_mode: 'fork'
+    },
+    {
+      name: 'laravel-queue-broadcast',
+      script: 'php',
+      args: ['artisan', 'queue:work', '--queue=default', '--verbose', '--tries=3', '--max-time=3600'],
+      cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
       autorestart: true,
       max_restarts: 10,
       min_uptime: '10s',
@@ -12,7 +25,18 @@ module.exports = {
     {
       name: 'laravel-reverb',
       script: 'php',
-      args: 'artisan reverb:start',
+      args: ['artisan', 'reverb:start'],
+      cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+      instances: 1,
+      exec_mode: 'fork'
+    },
+    {
+      name: 'crash-game-loop',
+      script: 'php',
+      args: ['artisan', 'crash:run'],
       cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
       autorestart: true,
       max_restarts: 10,
