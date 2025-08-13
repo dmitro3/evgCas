a0001752a110dacd2a160e076d1518099d3828a8
work <nikita@MacBookPro.domrurouter.net>
2025-08-13 17:09:37 +0500

    last

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,25 +1,26 @@
 module.exports = {
-    apps: [
-      {
-        name: 'laravel-queue',
-        script: './queue-worker.sh',
-        cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
-        autorestart: true,
-        max_restarts: 10,
-        min_uptime: '10s',
-        instances: 1,
-        exec_mode: 'fork'
-      },
-      {
-        name: 'laravel-reverb',
-        script: 'php',
-        args: 'artisan reverb:start',
-        cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
-        autorestart: true,
-        max_restarts: 10,
-        min_uptime: '10s',
-        instances: 1,
-        exec_mode: 'fork'
-      }
-    ]
-  };
+  apps: [
+    {
+      name: 'laravel-queue',
+      script: 'php',
+      args: ['artisan', 'queue:work', '--queue=wallet-generation,default', '--sleep=3', '--tries=3', '--max-time=3600', '--verbose'],
+      cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+      instances: 1,
+      exec_mode: 'fork'
+    },
+    {
+      name: 'laravel-reverb',
+      script: 'php',
+      args: ['artisan', 'reverb:start'],
+      cwd: '/var/www/ndstudiotest_usr/data/www/ndstudiotestdomain.cfd',
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+      instances: 1,
+      exec_mode: 'fork'
+    }
+  ]
+};
